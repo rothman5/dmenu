0ec18660d9cb4c747e338abe2d16be1a7c79331d
rothman5 <rothman@ualberta.ca>
2022-08-01 02:34:52 -0600

    updated config

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Source Code Pro:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                  fg         bg     */
+	[SchemeNorm] = { "#d8dee9", "#2b2b2b" },
+	[SchemeSel] = { "#eceff4", "#4c4c4c" },
+	[SchemeOut] = { "#2b2b2b", "#5b5b5b" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
